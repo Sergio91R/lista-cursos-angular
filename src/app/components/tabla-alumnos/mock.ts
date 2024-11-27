f40ecb40ea2df807ce41a22a8cdf1888daaea090
Sergio91R <sergio.rodriguez118@gmail.com>
2024-11-27 13:26:28 -0500

    Tablas en bootstrap de alumons y cursos para practicar Ts

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,17 @@
+export const alumnos = [
+    {
+        nombre:'Ricardo',
+        apellido:'Perez',
+        promedio:8.5
+    },
+    {
+        nombre:'luis',
+        apellido:'Torres',
+        promedio:7.5 
+    },
+    {
+        nombre:'Carolina',
+        apellido:'Cordoba',
+        promedio:6
+    }
+]
